1bbb4c5bda1fcf8cd2d5d0b20b61da257fd611c0
Elena-Yuki <helena-nelly@mail.ru>
2024-08-23 23:15:04 +0800

    Add files via upload

---
+++ module1_5.py
@@ -0,0 +1,9 @@
+# 1
+immutable_var = 1, 2, 3, 4, "Item", "Note", False
+print(immutable_var)
+print(type(immutable_var))
+mutable_list = [1, 2, 3, 4, 'alfa', "beta", "fly"]
+print(mutable_list)
+mutable_list[4] = "flower"
+print(mutable_list) 
+
