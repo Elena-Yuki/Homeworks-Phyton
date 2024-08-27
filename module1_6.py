4659f2093723a94a4ae36f75aa99cee326e3d9a4
Elena-Yuki <helena-nelly@mail.ru>
2024-08-27 18:02:03 +0800

    Add files via upload

--- module1_6.py
+++ module1_6.py
@@ -1,18 +1,18 @@
 # Словари
 my_dict = {"Food": "eggplant", "Friends": "Dariya", "numbers":89645321098, "cities": "Волгоград"}
 print(my_dict)
-my_dict["Food"]="carrots"
+print(my_dict.get("cities"))
+print(my_dict.get("Language", 1000))
+my_dict.update({"Students": "Jimmy", "Years": 2002 })
 print(my_dict)
-my_dict["languages"] = "Spanish"
-print(my_dict)
-my_dict.update({"Books": "White fang", "Continents": "Australia"})
-print(my_dict)
-del my_dict["cities"]
+f = my_dict.pop("Food")
+print(f)
 print(my_dict)
 # Множества
 my_set = {8, 7, 0, 76, 8, 7, 76, "wanted", (4, 5, 6)}
 print(my_set)
 print(my_set.add(87))
+print(my_set.add("apple"))
 print(my_set)
 print(my_set.remove(8))
 print(my_set)
