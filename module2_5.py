ae9d74f5169c319b77e1c2fb2065434143782d6e
Elena-Yuki <helena-nelly@mail.ru>
2024-08-31 23:14:25 +0800

    Add files via upload

--- module2_5.py
+++ module2_5.py
@@ -5,18 +5,17 @@ def get_matrix(n, m, value):
         for j in range (m):
             row.append(value)
             matrix.append(row)
-            return matrix
-        get_matrix (2, 2,10)
-        get_matrix (3, 5, 42)
-        get_matrix (4, 2, 13)
+        return matrix
+get_matrix (2, 2,10)
+get_matrix (3, 5, 42)
+get_matrix (4, 2, 13)
 
-        result1 = get_matrix(2,2,10)
-        result2 = get_matrix(3, 5, 43)
-        result3 = get_matrix(4, 2, 13)
-
-        print (result1)
-        print (result2)
-        print (result3)
+result1 = get_matrix(2,2,10)
+result2 = get_matrix(3, 5, 43)
+result3 = get_matrix(4, 2, 13)
+print (result1)
+print (result2)
+print (result3)
 
 
 
