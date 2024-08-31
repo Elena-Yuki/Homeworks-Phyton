10af294f995fb06c9ba879698f5a9a3dda7d0831
Elena-Yuki <helena-nelly@mail.ru>
2024-08-31 23:16:58 +0800

    Add files via upload

--- module2_5.py
+++ module2_5.py
@@ -1,10 +1,10 @@
 def get_matrix(n, m, value):
     matrix=[]
     for i in range (n):
-        row = []
+        stack = []
         for j in range (m):
-            row.append(value)
-            matrix.append(row)
+            stack.append(value)
+            matrix.append(stack)
         return matrix
 get_matrix (2, 2,10)
 get_matrix (3, 5, 42)
