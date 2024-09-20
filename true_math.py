4943a4a5f019ff37a85b6f33e0c0f703c8d5ffe6
Elena-Yuki <helena-nelly@mail.ru>
2024-09-20 22:34:52 +0800

    Add files via upload
    Задача "А как делить?":

---
+++ true_math.py
@@ -0,0 +1,21 @@
+from math import inf
+def divide (first, second):
+    if second == 0:
+        result = inf
+    else:
+        result = first/second
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
