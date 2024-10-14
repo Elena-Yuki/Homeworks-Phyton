12070941fb74e17c614b5cfa07e4d7a6777d78f2
Elena-Yuki <helena-nelly@mail.ru>
2024-10-14 16:02:40 +0800

    Задание "Программистам всё можно"

---
+++ module8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a,b):
+    try:
+        result = a + b
+        return result
+    except TypeError:
+        return str(a) + str(b)
+
+    finally:
+        print("Programmers can do everything if they wamt") 
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
