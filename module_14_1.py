3ede3c1499e13cfce032289a4dffe8c62e98fc72
Elena-Yuki <helena-nelly@mail.ru>
2024-12-13 22:59:00 +0800

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -13,19 +13,23 @@ balance INTEGER NOT NULL)
 ''')
 cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 
-for i in range(10):
-    cursor.execute ('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
-                  (f'User{i + 1}', f'example{i + 1}gmail.com', f'{(i + 1)*10}', '1000'))
-for i in range(1, 11, 2):
-    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{i}'))
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age,balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}',  f'example{i}@gmail.com', f'{i * 10}', 1000))
 
-for i in range(1, 11, 2):
-    cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+for i in range(1, 11):
+    if i % 2:
+        cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{i}'))
 
-cursor.execute('SELECT * FROM Users WHERE age != 60')
+cursor.execute('DELETE FROM Users WHERE (id-1) % 3 = 0')
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
 result = cursor.fetchall()
+
 for user in result:
-    print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
 
 connection.commit()
 connection.close()
+
+
